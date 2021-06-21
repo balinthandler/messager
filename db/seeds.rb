62f814795e564a699da0354bd6f0a5f2b846474a
Balint Handler <handlerbalint@gmail.com>
2021-06-21 21:26:44 +0200

    added bulma-rails, connection verification

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: 'johndoe@example.com', password: '123456')
+User.create(email: 'balint@example.com', password: '123456')
